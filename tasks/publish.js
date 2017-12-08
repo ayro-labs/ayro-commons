f9a67274944e35c5aefbc4c15fb96bb63f04cd32
Sandro Simas <sandro.csimas@gmail.com>
2017-12-08 02:25:01 -0300

    Release 0.0.5

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,31 +1,31 @@
 const projectPackage = require('../package');
-const utils = require('./utils');
+const {commands} = require('../lib');
 const path = require('path');
 const Promise = require('bluebird');
 
 const WORKING_DIR = path.resolve(__dirname, '../');
 
 function exec(command, dir) {
-  return utils.exec(command, dir || WORKING_DIR);
+  return commands.exec(command, dir || WORKING_DIR);
 }
 
 function checkoutTag(version) {
   return Promise.coroutine(function* () {
-    utils.log(`Checking out the tag ${version}...`);
+    commands.log(`Checking out the tag ${version}...`);
     yield exec(`git checkout ${version}`);
   })();
 }
 
 function lintLibrary() {
   return Promise.coroutine(function* () {
-    utils.log('Linting library...');
+    commands.log('Linting library...');
     yield exec('npm run lint');
   })();
 }
 
 function publishToNpm() {
   return Promise.coroutine(function* () {
-    utils.log('Publishing to Npm...');
+    commands.log('Publishing to Npm...');
     yield exec('npm publish');
   })();
 }
@@ -35,14 +35,14 @@ if (require.main === module) {
   Promise.coroutine(function* () {
     try {
       const {version} = projectPackage;
-      utils.log(`Publishing version ${version} to Github and Npm...`);
+      commands.log(`Publishing version ${version} to Github and Npm...`);
       yield checkoutTag(version);
       yield lintLibrary();
       yield publishToNpm();
       yield checkoutTag('master');
-      utils.log(`Version ${version} published with success!`);
+      commands.log(`Version ${version} published with success!`);
     } catch (err) {
-      utils.logError(err);
+      commands.logError(err);
       process.exit(1);
     }
   })();
