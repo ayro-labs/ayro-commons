cb5404b30118ef49e8e8bbe90cd90d88312c1677
Sandro Simas <sandro.csimas@gmail.com>
2018-05-25 02:01:00 -0300

    Improving publish.js; Sorting entries in gitignore

--- tasks/publish.js
+++ tasks/publish.js
@@ -10,10 +10,15 @@ async function lintProject() {
   await commands.exec('npm run lint', WORKING_DIR);
 }
 
+async function buildProject() {
+  // Nothing to do...
+}
+
 // Run this if call directly from command line
 if (require.main === module) {
   publish.withWorkingDir(WORKING_DIR);
-  publish.withBuildTask(lintProject);
+  publish.withLintTask(lintProject);
+  publish.withBuildTask(buildProject);
   publish.isNpmProject(true);
   publish.run();
 }
